12cdac2ca8d8cb68dec4d9c7572eba38b13f1dff
LavieroMancinelli <mancinelli.laviero@gmail.com>
2025-11-01 23:08:22 +0000

    added users to index.ts, updated .env for neon (and .env.example)

--- harmony-backend/src/index.ts
+++ harmony-backend/src/index.ts
@@ -1,18 +1,26 @@
  import express, {Request, Response} from "express";
  import cors from "cors";
  import dotenv from "dotenv";
+ import { PrismaClient } from "@prisma/client";
 
  dotenv.config();
  const app = express();
+ const prisma = new PrismaClient();
 
  app.use(cors());
  app.use(express.json());
 
  app.get("/", (_req: Request, res: Response) => {
-    res.send("Harmony backend running");
+   res.send("Harmony backend running");
+ })
+
+ app.get("/users", async (_req: Request, res: Response) => {
+   const users = await prisma.user.findMany();
+   res.json(users);
  });
 
  const PORT = process.env.PORT || 4000;
+
  app.listen(PORT, () => {
-    console.log('Server running on port${PORT}');
+    console.log('Server running on port ${PORT}');
  });
